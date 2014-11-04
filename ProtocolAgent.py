49974fcd301ac1e4acfc0f303827defca40dd260
garfield <garfield@garfields-MacBook-Pro.local>
2014-11-04 08:14:30 +0800

    step

--- ProtocolAgent.py
+++ ProtocolAgent.py
@@ -26,18 +26,67 @@ class Hub:
         self.hooks_in = hooks_in
         self.hooks_out = hooks_out
 
+        self.agents = []
+
         self.server = StreamServer(('0.0.0.0', port), self.on_new_connection)
 
-    def serve_forever(self):
-        self.server.serve_forever()
+    def start(self):
+        self.server.start()
 
     def on_new_connection(self, sock, address):
         try:
             agent = self.agent_class(sock, address, self.addr_out, self, self.hooks_in, self.hooks_out)
-            agent.update()
+            self.register(agent)
         except IOError:
             sock.close()
 
+    def register(self, agent):
+        self.agents.push_back(agent)
+
+
+class PackSocket:
+    loop = gevent.get_hub().loop
+
+    def __init__(self, sock, head_len, on_pack):
+        self.write_queue = []
+
+        self.read_buff = ""
+        self.pack_len = -1
+
+        self.sock = sock
+        self.head_len = head_len
+        self.on_pack = on_pack
+
+        self.read_watcher = self.loop.io(sock.fileno, 1)
+        self.read_watcher.priority = self.loop.MAXPRI
+        self.read_watcher.start(self.__on_readable)
+
+        self.write_watcher = self.loop.io(sock.fileno, 2)
+        self.write_watcher.priority = self.loop.MAXPRI
+        self.write_watcher.start(self.__on_writable)
+
+    def write(self, data):
+        self.write_queue.push(data)
+
+    def close(self):
+        self.read_watcher.stop()
+        self.write_watcher.stop()
+
+    def __on_writable(self):
+        pass
+
+    def __on_readable(self):
+        len_l = self.head_len - len(self.read_buff)
+        if len_l > 0:
+            self.read_buff += self.sock.read(self.head_len)
+
+        if self.pack_len > 0:
+            self.read_buff += self.sock.read(self.pack_len - len(self.read_buff) - self.head_len)
+
+        if len(self.read_buff) == self.pack_len - self.head_len:
+            self.on_pack(self.read_buff[self.head_len:])
+            self.read_buff = ""
+            self.pack_len = -1
 
 class Agent:
     def __init__(self, sock_in, addr_in, addr_out, hooks_in, hooks_out, hub):
@@ -47,11 +96,12 @@ class Agent:
         self.hook_in = hooks_in
         self.hook_out = hooks_out
 
+        self.hub = hub
+
         self.sock_out = gevent.socket.create_connection(addr_out)
 
         if not self.sock_out:
             raise IOError()
-            pass
 
     def message_in(self, msg_id, data):
         self.hook_message(self.socke_out, self.hooks_in, msg_id, data)
--- main.py
+++ main.py
@@ -14,5 +14,7 @@ hooks_out = {
 
 if __name__ == "__main__":
     sandboxServer = FlashSandbox.sandbox(843)
+    sandboxServer.start()
     mgr = ProtocolAgent.Hub(2200, ("192.168.1.78", 2256), hooks_in, hooks_out, JsonAgent)
+    mgr.start()
     mgr.serve_forever()
